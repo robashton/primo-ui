54b2d2a47ede9a53afeb1f9abef224f9023dac18
Rob Ashton <robashton@codeofrob.com>
2013-01-29 13:24:29 +0000

    Added default 'label' UI object

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var PrimoUi = require('./container')
 var Rect = require('./rect')
+var Label = require('./label')
 
 module.exports = {
   init: function(game, options) {
     game.ui = new PrimoUi(game, options)
   },
-  Rect: Rect
+  Rect: Rect,
+  Label: Label
 }
---
+++ label.js
@@ -0,0 +1,20 @@
+var util = require('primo-utils')
+
+var Label = function(data) {
+  this.x = data.x
+  this.y = data.y
+  this.height = data.height
+  this.font = util.valueOrDefault(data.font, 'sans-serif')
+  this.colour = util.valueOrDefault(data.colour, '#F00')
+  this.text = util.valueOrDefault(data.text, "") 
+}
+
+Label.prototype = {
+  render: function(context) {
+    context.fillStyle = this.colour
+    context.font = this.height + 'px ' + this.font
+    context.fillText(this.text, this.x, this.y + this.height)
+  }
+}
+
+module.exports = Label
