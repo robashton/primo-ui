5ee2fd7d3d571682418d4b2aa7c11e2a511df7aa
Rob Ashton <robashton@codeofrob.com>
2013-01-29 17:13:58 +0000

    Added alignment options to label, that'll come back to bite me

--- label.js
+++ label.js
@@ -7,11 +7,13 @@ var Label = function(data) {
   this.font = util.valueOrDefault(data.font, 'sans-serif')
   this.colour = util.valueOrDefault(data.colour, '#F00')
   this.text = util.valueOrDefault(data.text, "") 
+  this.align = util.valueOrDefault(data.align, "left")
 }
 
 Label.prototype = {
   render: function(context) {
     context.fillStyle = this.colour
+    context.textAlign = this.align
     context.font = this.height + 'px ' + this.font
     context.fillText(this.text, this.x, this.y + this.height)
   }
