cdb086c83283f0d84dc0b5c7d23d1b1923197bb0
Rob Ashton <robashton@codeofrob.com>
2013-01-30 12:36:20 +0000

    Hide and show

--- label.js
+++ label.js
@@ -8,14 +8,22 @@ var Label = function(data) {
   this.colour = util.valueOrDefault(data.colour, '#F00')
   this.text = util.valueOrDefault(data.text, "") 
   this.align = util.valueOrDefault(data.align, "left")
+  this.hidden = false
 }
 
 Label.prototype = {
   render: function(context) {
+    if(this.hidden) return
     context.fillStyle = this.colour
     context.textAlign = this.align
     context.font = this.height + 'px ' + this.font
     context.fillText(this.text, this.x, this.y + this.height)
+  },
+  hide: function() {
+    this.hidden = true
+  },
+  show: function() {
+    this.hidden = false
   }
 }
 
--- rect.js
+++ rect.js
@@ -4,12 +4,20 @@ var Rect = function(data) {
   this.width = data.width
   this.height = data.height
   this.colour = data.colour
+  this.hidden = false
 }
 
 Rect.prototype = {
   render: function(context) {
+    if(this.hidden) return
     context.fillStyle = this.colour
     context.fillRect(this.x, this.y, this.width, this.height)
+  },
+  hide: function() {
+    this.hidden = true
+  },
+  show: function() {
+    this.hidden = false
   }
 }
 
